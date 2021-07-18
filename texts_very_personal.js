d2e5865ffd8f2b8c35c207fa2a5145dbbab1800e
wellthatsummokay <reinastreufert@gmail.com>
2021-07-17 22:54:11 -0400

    Create texts_very_personal.js

---
+++ texts_very_personal.js
@@ -0,0 +1,11 @@
+(function() {
+	window.texts[15] = "all he knows how to do is punch. but mmmmm look at those muscles (haha get it im being you)";
+
+	window.texts[29] = "yknow... they say those who are killed by virgins become virgins.";
+	window.texts[30] = "i think i can help fix that if you want ;)";
+
+	window.texts[31] = "OMG OMG you killed ed sheeran youre so hot rn";
+	window.texts[32] = "killing ed sheeran makes you v sexy";
+	window.texts[33] = "go send me nudes rn do it or dont thats okay too i dont wanna make you feel pressured but fucking do it bitch but again only if youre comfortable consent is a very important thing to have in a relationship and i would never want to make you do something you're not comfortable with so please tell me if i ever make you feel that way anyways send me nudes you dumb bitch but fr i love you so much and i always want you to feel comfortable and safe so if you dont want to thats completely okay and i would never make you do that like seriously i mean that with all my heart i love you so much now show me your fucking tits";
+	window.texts[34] = "ANYWAYS you get to have a nice break before the final boss so enjoy that :)";
+})();
